068d5b0a7b4946827241e3a3c304f38d375c4c9b
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-01-06 13:31:45 +0100

    Improvements to the step verifier

--- processors/src/main/kotlin/nocucumber/internal/step/StepVerifier.kt
+++ processors/src/main/kotlin/nocucumber/internal/step/StepVerifier.kt
@@ -8,7 +8,7 @@ import javax.lang.model.element.Element
 import javax.lang.model.element.ElementKind
 
 internal class StepVerifier(messager: Messager) : AnnotationVerifier<Step>(messager) {
-    private var stepsFound = listOf<String>()
+    private var stepsFound = mapOf<String, Element>()
 
     override fun verify(element: Element, annotation: Step) = when {
         annotation.names.filterNot { it.isBlank() }.isEmpty() -> {
@@ -23,12 +23,14 @@ internal class StepVerifier(messager: Messager) : AnnotationVerifier<Step>(messa
             err("Step elements must be annotated with @${Test::class.java.canonicalName}", element)
             false
         }
-        annotation.names.any { it in stepsFound } -> {
-            err("Step ${annotation.names.first { it in stepsFound }} already added somewhere else", element)
+        annotation.names.any { it in stepsFound.keys } -> {
+            val step = annotation.names.first { it in stepsFound.keys }
+            err("Step $step already added by ${stepsFound[step]!!.apply {
+                "${enclosingElement.simpleName}#$simpleName"}}}", element)
             false
         }
         else -> {
-            annotation.names.forEach { stepsFound += it }
+            annotation.names.forEach { stepsFound += it to element }
             true
         }
     }
