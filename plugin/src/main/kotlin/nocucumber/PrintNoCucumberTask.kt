d410f5814ff55689de2788bec7632a9b18419a89
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-01-06 16:18:41 +0100

    Print StandardLocation.SOURCE_OUTPUT

--- plugin/src/main/kotlin/nocucumber/PrintNoCucumberTask.kt
+++ plugin/src/main/kotlin/nocucumber/PrintNoCucumberTask.kt
@@ -2,11 +2,15 @@ package nocucumber
 
 import org.gradle.api.Project
 import org.gradle.api.Task
+import java.nio.file.Paths
 
 internal class PrintNoCucumberTask : NoCucumberTask {
     override fun name() = "noCucumberPrint"
 
     override fun apply(project: Project, task: Task) {
-        // TODO
+        System.out.println(Paths.get(project.buildDir.absolutePath,
+                "generated", "source", *langSpecificPath(), "nocucumber"))
     }
+
+    private fun langSpecificPath() = arrayOf("kapt", "debugAndroidTest")
 }
