afb3a0bbc88b886e5901c9f2e33ebb21b21b85ef
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-01-06 22:39:37 +0100

    Prepare processor-specific path

--- plugin/src/main/kotlin/nocucumber/PrintNoCucumberTask.kt
+++ plugin/src/main/kotlin/nocucumber/PrintNoCucumberTask.kt
@@ -1,16 +1,34 @@
 package nocucumber
 
+import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.Task
+import java.nio.file.Path
 import java.nio.file.Paths
 
 internal class PrintNoCucumberTask : NoCucumberTask {
     override fun name() = "noCucumberPrint"
 
     override fun apply(project: Project, task: Task) {
-        System.out.println(Paths.get(project.buildDir.absolutePath,
-                "generated", "source", *langSpecificPath(), "nocucumber"))
+        System.out.println("Applying PrintNoCucumberTask")
     }
 
-    private fun langSpecificPath() = arrayOf("kapt", "debugAndroidTest")
+    private fun jsonStepCollectionFile(project: Project) =
+            Paths.get(langSpecificPath(project), "nocucumber", "json", "steps.json").toFile()
+
+    private fun langSpecificPath(project: Project): String {
+        val kotlinPath =
+                Paths.get(project.buildDir.absolutePath, "generated", "source", "kapt", "debugAndroidTest")
+        val javaPath =
+                Paths.get(project.buildDir.absolutePath, "generated", "source", "apt", "debugAndroidTest")
+        return if (kotlinPath.toFile().exists()) {
+            kotlinPath
+        } else {
+            if (javaPath.toFile().exists()) {
+                javaPath
+            } else {
+                throw GradleException("Did not found Kotlin or Java generated source folders")
+            }
+        }.toAbsolutePath().toString()
+    }
 }
