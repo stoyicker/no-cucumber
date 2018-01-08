af3848ef651bbdc23c760054176296f98967b1ed
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-01-08 19:24:41 +0100

    Generate feature index

--- plugin/src/main/kotlin/nocucumber/internal/testreports/HtmlReportGenerator.kt
+++ plugin/src/main/kotlin/nocucumber/internal/testreports/HtmlReportGenerator.kt
@@ -2,6 +2,7 @@ package nocucumber.internal.testreports
 
 import java.io.File
 import java.nio.file.Paths
+import java.util.*
 
 internal class HtmlReportGenerator {
     fun generate(deviceReport: NoCucumberDeviceReport, targetFile: File) {
@@ -37,8 +38,37 @@ internal class HtmlReportGenerator {
         }
     }
 
-    private fun applyReplacements(deviceReport: NoCucumberDeviceReport, template: String)=
-        applyProjectReplacement(deviceReport, applyDeviceReplacement(deviceReport, template))
+    private fun applyReplacements(deviceReport: NoCucumberDeviceReport, template: String) =
+            applyFeatureReportReplacement(deviceReport,
+                    applyFeatureIndexReplacement(deviceReport,
+                            applyProjectReplacement(deviceReport,
+                                    applyDeviceReplacement(deviceReport, template))))
+
+    private fun applyFeatureReportReplacement(deviceReport: NoCucumberDeviceReport, template: String): String {
+        val featureReportTemplate =
+                HtmlReportGenerator::class.java
+                        .getResource("/test-report-template/feature-report.html")
+                        .readText(Charsets.UTF_8)
+        val featureReport = deviceReport.featureReports.joinToString(separator = "\n") {
+            featureReportTemplate
+                    .replace("\${FEATURE_ID}", it.name.toLowerCase(Locale.ENGLISH).replace(" ", "_"))
+                    .replace("\${FEATURE_NAME}", it.name)
+        }
+        return template.replace("\${FEATURE_REPORT}", featureReport)
+    }
+
+    private fun applyFeatureIndexReplacement(deviceReport: NoCucumberDeviceReport, template: String): String {
+        val featureIndexTemplate =
+                HtmlReportGenerator::class.java
+                        .getResource("/test-report-template/feature-index.html")
+                        .readText(Charsets.UTF_8)
+        val featureIndex = deviceReport.featureReports.joinToString(separator = "\n") {
+            featureIndexTemplate
+                    .replace("\${FEATURE_ID}", it.name.toLowerCase(Locale.ENGLISH).replace(" ", "_"))
+                    .replace("\${FEATURE_NAME}", it.name)
+        }
+        return template.replace("\${FEATURE_INDEX}", featureIndex)
+    }
 
     private fun applyProjectReplacement(deviceReport: NoCucumberDeviceReport, template: String) =
             template.replace("\${PROJECT}", deviceReport.project)
